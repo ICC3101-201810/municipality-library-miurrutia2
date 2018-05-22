892015b605d0d1a148cb1577afe8c5d2e10cff07
ING <safernandez1@miuandes.cl>
2018-05-22 18:29:58 -0400

    Lab10

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 clase = new Class1();
+            Assembly assembly = Assembly.LoadFrom("ClassLibrary1.dll");
+            Type claseType = clase.GetType();
+
+            PropertyInfo[] properties = claseType.GetProperties();
+            MethodInfo[] methods = claseType.GetMethods();
+        }
+    }
+}
