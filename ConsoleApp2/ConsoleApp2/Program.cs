d196d8b63f200fdc1618fd38bcb851ef126e1a68
Ignacia Urrutia <ignaciaurrutia@MacBook-Pro-de-Ignacia-2.local>
2018-05-22 22:12:40 -0400

    Lab10

--- ConsoleApp2/ConsoleApp2/Program.cs
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -4,15 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Reflection;
+using System.IO;
 using ClassLibrary1;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ConsoleApp2
 {
     [Serializable]
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
+
             List<Car> Auto = new List<Car>();
             List<Person> Persona = new List<Person>();
             List<Address> Direccion = new List<Address>();
@@ -21,29 +24,39 @@ namespace ConsoleApp2
             Console.WriteLine("3. Para inscribir una propiedad");
             string Opcion = Console.ReadLine();
 
+
             if (Opcion == "1")
             {
-                Console.WriteLine("Ingrese la marca");
-                string brand = Console.ReadLine();
-                Console.WriteLine("Ingrese el modelo");
-                string model = Console.ReadLine();
-                Console.WriteLine("Ingrese el año");
-                int year = Convert.ToInt32(Console.ReadLine());
-                //Console.WriteLine("Ingrese el nombre del dueño");
-                //Person owner = Console.ReadLine();
-                Console.WriteLine("Ingrese la patente");
-                string licencePlate = Console.ReadLine();
-                Console.WriteLine("Ingrese el numero de cinturones");
-                int seatbelt = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("Ingrese si su vehiculo es deisel (1 para si y 2 para no");
-                bool diesel = false;
-                string bencina = Console.ReadLine();
-                if (bencina == "1")
+                Console.WriteLine("Ingrese 1 si quiere inscribir un auto, 2 si quiere cambiar de dueño");
+                string v = Console.ReadLine();
+                if (v == "1") 
                 {
-                    diesel = true;
+                    Console.WriteLine("Ingrese la marca");
+                    string brand = Console.ReadLine();
+                    Console.WriteLine("Ingrese el modelo");
+                    string model = Console.ReadLine();
+                    Console.WriteLine("Ingrese el año");
+                    int year = Convert.ToInt32(Console.ReadLine());
+                    //Console.WriteLine("Ingrese el nombre del dueño");
+                    //Person owner = Console.ReadLine();
+                    Console.WriteLine("Ingrese la patente");
+                    string licencePlate = Console.ReadLine();
+                    Console.WriteLine("Ingrese el numero de cinturones");
+                    int seatbelt = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Ingrese si su vehiculo es deisel (1 para si y 2 para no");
+                    bool diesel = false;
+                    string bencina = Console.ReadLine();
+                    if (bencina == "1")
+                    {
+                        diesel = true;
+                    }
+                    Car auto = new Car(brand, model, year, null, licencePlate, seatbelt, diesel);
+                    Auto.Add(auto);
                 }
-                Car auto = new Car(brand, model, year, null, licencePlate, seatbelt, diesel);
-                Auto.Add(auto);
+                else if (v == "2")
+                {
+                }
+
 
             }
 
@@ -105,7 +118,116 @@ namespace ConsoleApp2
                 Direccion.Add(direccion);
             }
 
+            //void SerializablePerson(List<Person> persona)
+            //{
+              //  try
+                //{
+                  //  using (Stream st = File.Open("../../Person.bin", FileMode.Create))
+                    //{
+                      //  BinaryFormatter bf = new BinaryFormatter();
+                        //bf.Serialize(st, Persona);
+                    //}
+                //}
+                //catch (Exception ex)
+                //{
+                    
+                //}
 
+            //}
+
+            //List<Person> DeserealizarPerson()
+            //{
+                
+              //  try
+                //{
+                  //  using (Stream st = File.Open("../../Person.bin", FileMode.Open))
+                    //{
+                       // BinaryFormatter bf = new BinaryFormatter();
+                        //Persona = (List<Person>)bf.Deserialize(st);
+                   // }
+                    //return Persona;
+
+                //}
+                //catch (Exception ex)
+                //{
+
+                  //  return null;
+                //}
+            //}
+
+            //void SerializableCar(List<Car> auto)
+            //{
+              //  try
+                //{
+                  //  using (Stream st = File.Open("../../Car.bin", FileMode.Create))
+                   // {
+                     //   BinaryFormatter bf = new BinaryFormatter();
+                       // bf.Serialize(st, Auto);
+                    //}
+                //}
+                //catch (Exception ex)
+                //{
+
+                //}
+
+            //}
+
+            //List<Person> DeserealizarCar()
+            //{
+              //  List<Car> Auto;
+                //try
+                //{
+                  //  using (Stream st = File.Open("../../Car.bin", FileMode.Open))
+                    //{
+                      //  BinaryFormatter bf = new BinaryFormatter();
+                        //Auto = (List<Car>)bf.Deserialize(st);
+                    //}
+                    //return Auto;
+
+                //}
+                //catch (Exception ex)
+                //{
+
+                  //  return null;
+                //}
+            //}
+
+            //void SerializableAddress(List<Address> direccion)
+            //{
+              //  try
+                //{
+                  //  using (Stream st = File.Open("../../Address.bin", FileMode.Create))
+                    //{
+                      //  BinaryFormatter bf = new BinaryFormatter();
+                        //bf.Serialize(st, Direccion);
+                  //  }
+                //}
+                //catch (Exception ex)
+                //{
+
+                //}
+
+            //}
+
+            //List<Person> DeserealizarAddress()
+            //{
+
+              //  try
+                //{
+                  //  using (Stream st = File.Open("../../Address.bin", FileMode.Open))
+                    //{
+                      //  BinaryFormatter bf = new BinaryFormatter();
+                        //Direccion = (List<Address>)bf.Deserialize(st);
+                    //}
+                    //return Direccion;
+
+                //}
+                //catch (Exception ex)
+                //{
+
+                  //  return null;
+                //}
+           // }
         }
 
 
